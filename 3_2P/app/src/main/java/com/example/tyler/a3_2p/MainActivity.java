c609ddea83c6d115e95d62eb07e02bdae85b593b
Tyler <tyler-smith@live.com.au>
2018-08-24 19:08:30 +1000

    non working 3_2P

--- 3_2P/app/src/main/java/com/example/tyler/a3_2p/MainActivity.java
+++ 3_2P/app/src/main/java/com/example/tyler/a3_2p/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.tyler.a3_2p;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button convertInches = findViewById(R.id.buttonInches);
+        convertInches.setOnClickListener(convertInchesListen);
+    }
+
+    /*convert inches listener*/
+    View.OnClickListener convertInchesListen = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            EditText inputInch = findViewById(R.id.inchesText);
+            TextView convertInch = findViewById(R.id.centimeters);
+            String dInch = changeInches(inputInch.getText().toString());
+            convertInch.setText(dInch);
+        }
+    };
+
+
+    /*convert inches*/
+    public String changeInches(String dCm)
+    {
+        try {
+            double cm = Double.parseDouble(dCm);
+            double inch = cm*(2.54);
+            return String.format(3.3f, inch);
+        }
+        catch (NumberFormatException nfe)
+        {
+            return "error with input";
+        }
     }
 }
