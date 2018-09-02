5f12c30167cf76bf34081094c5b21477640377c4
Even <tyler-smith@live.com.au>
2018-09-02 23:20:08 +1000

    updated buttons

--- 3_2P/app/src/main/java/com/example/tyler/a3_2p/MainActivity.java
+++ 3_2P/app/src/main/java/com/example/tyler/a3_2p/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -31,9 +32,12 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v)
         {
             convertInch();
+            convertFeet();
+            convertMiles();
         }
     };
 
+    /*convert inches only*/
     public void convertInch()
     {
         EditText inputInch = findViewById(R.id.inchesText);
@@ -41,28 +45,52 @@ public class MainActivity extends AppCompatActivity {
         TextView convertInch = findViewById(R.id.centimeters);
         convertInch.setText(dInch);
     }
+
+    public void convertFeet()
+    {
+        EditText inputFeet = findViewById(R.id.feetText);
+        String dFeet = convertImperialCm(inputFeet.getText().toString(), "feet");
+        TextView convertFeet = findViewById(R.id.feet);
+        convertFeet.setText(dFeet);
+    }
+
+    public void convertMiles()
+    {
+        EditText inputMiles = findViewById(R.id.milesText);
+        String dMiles = convertImperialCm(inputMiles.getText().toString(), "miles");
+        TextView convertMiles = findViewById(R.id.miles);
+        convertMiles.setText(dMiles);
+    }
     /*convert imperial to centimeters given imperial type*/
     public String convertImperialCm(String dText, String type)
     {
+        try {
             switch (type) {
-                case "inch": {
-                    double cm = Double.parseDouble(dText);
-                    double inch = cm * (2.54);
-                    return String.format("%3.2f", inch);
+
+                    case "inch": {
+                        double cm = Double.parseDouble(dText);
+                        double inch = cm * (2.54);
+                        return String.format("%3.2f", inch);
+                    }
+                    case "feet": {
+                        double cm = Double.parseDouble(dText);
+                        double feet = cm * (2.54) * (12);
+                        return String.format("%3.1f", feet);
+                    }
+                    case "miles": {
+                        double cm = Double.parseDouble(dText);
+                        double miles = cm * (2.54) * (12) * (5280);
+                        return String.format("%1f", miles);
+                    }
+                    default:
+                        return "error with input";
                 }
-                case "feet": {
-                    double cm = Double.parseDouble(dText);
-                    double feet = cm * (2.54) * (12);
-                    return String.format("%3.2f", feet);
-                }
-                case "miles": {
-                    double cm = Double.parseDouble(dText);
-                    double miles = cm * (2.54) * (12) * (5280);
-                    return String.format("%3.2f", miles);
-                }
-                default: return "error with input";
+
             }
 
+            catch (NumberFormatException nfe)
+            {
+                return "ERR";
+            }
         }
-
 }
