e398bc88b8eb4714d3fe95542d049a713b17fc86
Even <tyler-smith@live.com.au>
2018-09-05 20:29:15 +1000

    finsihed 3_2P

--- 3_2P/app/src/main/java/com/example/tyler/a3_2p/MainActivity.java
+++ 3_2P/app/src/main/java/com/example/tyler/a3_2p/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,20 +15,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initialiseUI();
+        initialiseUI(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        EditText inchesText = findViewById(R.id.inchesText);
+        String saInches = inchesText.getText().toString();
+        outState.putString("Inch", saInches);
+
+        EditText feetText = findViewById(R.id.feetText);
+        String saFeet = feetText.getText().toString();
+        outState.putString("Feet", saFeet);
+
+        EditText milesText = findViewById(R.id.milesText);
+        String saMiles = milesText.getText().toString();
+        outState.putString("Miles", saMiles);
+
+        super.onSaveInstanceState(outState);
+    }
+
+    private void restoreState (Bundle state)
+    {
+        if (state == null)
+        {
+            return;
+        }
+        EditText inchesText = findViewById(R.id.inchesText);
+        String saInch = state.getString("Inch");
+        inchesText.setText(saInch);
+
+        EditText feetText = findViewById(R.id.feetText);
+        String saFeet = state.getString("Feet");
+        feetText.setText(saFeet);
+
+        EditText milesText = findViewById(R.id.milesText);
+        String saMiles = state.getString("Miles");
+        milesText.setText(saMiles);
 
     }
     /*User interface initialisation*/
-    public void initialiseUI()
+    public void initialiseUI(Bundle state)
     {
         /*Button Listener init*/
         Button convertButton = findViewById(R.id.convertButton);
         convertButton.setOnClickListener(convertButtList);
+        /*bundle restore*/
+        restoreState(state);
+        convertButton.callOnClick();
     }
 
     /*convert inches listener*/
-    View.OnClickListener convertButtList = new View.OnClickListener()
-    {
+    View.OnClickListener convertButtList = new View.OnClickListener() {
         @Override
         public void onClick(View v)
         {
@@ -41,56 +80,98 @@ public class MainActivity extends AppCompatActivity {
     public void convertInch()
     {
         EditText inputInch = findViewById(R.id.inchesText);
-        String dInch = convertImperialCm(inputInch.getText().toString(),"inch");
+        String dInch = convertInch(inputInch.getText().toString());
         TextView convertInch = findViewById(R.id.centimeters);
-        convertInch.setText(dInch);
+        CheckBox meterCheck = findViewById(R.id.convertType);
+        if (meterCheck.isChecked()) {
+            convertInch.setText(dInch + " m");
+        }
+        else {
+            convertInch.setText(dInch + " cm");
+        }
     }
 
     public void convertFeet()
     {
         EditText inputFeet = findViewById(R.id.feetText);
-        String dFeet = convertImperialCm(inputFeet.getText().toString(), "feet");
+        String dFeet = convertFeet(inputFeet.getText().toString());
         TextView convertFeet = findViewById(R.id.feet);
-        convertFeet.setText(dFeet);
+        CheckBox meterCheck = findViewById(R.id.convertType);
+        if (meterCheck.isChecked()) {
+            convertFeet.setText(dFeet + " m");
+        }
+        else {
+            convertFeet.setText(dFeet + " cm");
+        }
     }
 
     public void convertMiles()
     {
         EditText inputMiles = findViewById(R.id.milesText);
-        String dMiles = convertImperialCm(inputMiles.getText().toString(), "miles");
+        String dMiles = convertMiles(inputMiles.getText().toString());
         TextView convertMiles = findViewById(R.id.miles);
-        convertMiles.setText(dMiles);
+        CheckBox meterCheck = findViewById(R.id.convertType);
+        if (meterCheck.isChecked()) {
+            convertMiles.setText(dMiles + " m");
+        }
+        else {
+            convertMiles.setText(dMiles + " cm");
+        }
     }
     /*convert imperial to centimeters given imperial type*/
-    public String convertImperialCm(String dText, String type)
+    public String convertInch (String dText)
     {
         try {
-            switch (type) {
-
-                    case "inch": {
-                        double cm = Double.parseDouble(dText);
-                        double inch = cm * (2.54);
-                        return String.format("%3.2f", inch);
-                    }
-                    case "feet": {
-                        double cm = Double.parseDouble(dText);
-                        double feet = cm * (2.54) * (12);
-                        return String.format("%3.1f", feet);
-                    }
-                    case "miles": {
-                        double cm = Double.parseDouble(dText);
-                        double miles = cm * (2.54) * (12) * (5280);
-                        return String.format("%1f", miles);
-                    }
-                    default:
-                        return "error with input";
-                }
-
+            double cm = Double.parseDouble(dText);
+            double inch = cm * (2.54);
+            CheckBox meterCheck = findViewById(R.id.convertType);
+            if (meterCheck.isChecked())
+            {
+                inch *= 0.01;
             }
+            return String.format("%3.2f", inch);
+        }
 
             catch (NumberFormatException nfe)
             {
-                return "ERR";
+                return "0";
             }
+    }
+
+    public String convertFeet (String dText)
+    {
+        try{
+            double cm = Double.parseDouble(dText);
+            double feet = cm * (2.54) * (12);
+            CheckBox meterCheck = findViewById(R.id.convertType);
+            if (meterCheck.isChecked())
+            {
+                feet *= 0.01;
+            }
+            return String.format("%3.1f", feet);
         }
+        catch (NumberFormatException nfe)
+        {
+            return "0";
+        }
+
+    }
+    public String convertMiles (String dText)
+    {
+        try {
+            double cm = Double.parseDouble(dText);
+            double miles = cm * (2.54) * (12) * (5280);
+            CheckBox meterCheck = findViewById(R.id.convertType);
+            if (meterCheck.isChecked())
+            {
+                miles *= 0.01;
+            }
+            return String.format("%1.1f", miles);
+        }
+        catch (NumberFormatException nfe)
+        {
+            return "0";
+        }
+
+    }
 }
