f8475da57b4cd57a607e3df9d4b1637b6aafc647
Tyler <tyler-smith@live.com.au>
2018-09-26 17:20:08 +1000

    finished 4_2P, added temp convert and full app reqs

--- 4_1P/app/src/main/java/com/example/tyler/a4_1p/TemperatureConv.java
+++ 4_1P/app/src/main/java/com/example/tyler/a4_1p/TemperatureConv.java
@@ -2,6 +2,10 @@ package com.example.tyler.a4_1p;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TemperatureConv extends AppCompatActivity {
 
@@ -9,5 +13,63 @@ public class TemperatureConv extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature_conv);
+        initialiseUI(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        EditText tempText = findViewById(R.id.TempInput);
+        String saveTemp = tempText.getText().toString();
+        outState.putString("temp",saveTemp);
+
+        super.onSaveInstanceState(outState);
+    }
+
+
+    private void restoreState (Bundle state)
+    {
+        if (state == null)
+        {
+            return;
+        }
+        EditText tempText = findViewById(R.id.TempInput);
+        String savedTemp = state.getString("temp");
+        tempText.setText(savedTemp);
+
+    }
+
+    public void initialiseUI( Bundle state)
+    {
+        /*button listener for convert*/
+        Button convertButton = findViewById(R.id.convertTemp);
+        convertButton.setOnClickListener(convertButtonList);
+        /*bundle restore*/
+        restoreState(state);
+        convertButton.callOnClick();
+    }
+
+    View.OnClickListener convertButtonList = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v){
+
+            EditText inputTemp = findViewById(R.id.TempInput);
+            String dTemp = convertTemp(inputTemp.getText().toString());
+            TextView convertTemp = findViewById(R.id.TempOutput);
+            convertTemp.setText(dTemp + "F");
+        }
+
+    };
+
+
+    public String convertTemp(String sCelsius ) {
+        try {
+            double celsius = Double.parseDouble(sCelsius);
+            double fahrenheit = celsius * (9 / 5) + 32;
+            return String.format("%3.2f", fahrenheit);
+        } catch (NumberFormatException nfe) {
+            return "0";
+        }
     }
 }
