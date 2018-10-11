c8edbbd352c920ebcd95b62543add8258c395220
Tyler <tyler-smith@live.com.au>
2018-10-11 21:40:07 +1100

    completed 4_1P and 4_2P

--- 4_2P/app/src/main/java/com/example/tyler/a4_2p/ImageDisplayActivity.java
+++ 4_2P/app/src/main/java/com/example/tyler/a4_2p/ImageDisplayActivity.java
@@ -1,7 +1,10 @@
 package com.example.tyler.a4_2p;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ImageDisplayActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class ImageDisplayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_display);
+        initializeUI(savedInstanceState);
     }
+
+    public void initializeUI(Bundle savedInstanceState)
+    {
+        //get data from intent
+        Bundle mangoData = getIntent().getExtras();
+        Drawable mangoDrawn = getResources().getDrawable(mangoData.getInt("drawMango"));
+        String mangoDescr = mangoData.getString("nameMango");
+
+        //get objects on screen
+        ImageView activeImage = findViewById(R.id.activeImage);
+        TextView activeText = findViewById(R.id.activeText);
+
+        //set objects on screen
+        activeImage.setImageDrawable(mangoDrawn);
+        activeText.setText(mangoDescr);
+    }
+
 }
--- 4_2P/app/src/main/java/com/example/tyler/a4_2p/MainActivity.java
+++ 4_2P/app/src/main/java/com/example/tyler/a4_2p/MainActivity.java
@@ -16,26 +16,74 @@ public class MainActivity extends AppCompatActivity {
         initialiseUI();
     }
 
-    public void initialiseUI()
-    {
+    public void initialiseUI() {
+        //create click listeners for each image
         ImageView mango1 = findViewById(R.id.mango1);
         mango1.setOnClickListener(mango1list);
+        ImageView mango2 = findViewById(R.id.mango2);
+        mango2.setOnClickListener(mango2list);
+        ImageView mango3 = findViewById(R.id.mango3);
+        mango3.setOnClickListener(mango3list);
+        ImageView mango4 = findViewById(R.id.mango4);
+        mango4.setOnClickListener(mango4list);
     }
 
-    View.OnClickListener mango1list = new View.OnClickListener()
+    //click listeners for each image that send the information of what is clicked to the
+    // setActiveMango function
+    View.OnClickListener mango1list = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startMango1();
+        }
+    };
+
+    public void startMango1() {
+        int draw = R.drawable.mango1;
+        String name = getResources().getString(R.string.mango1);
+        setActiveMango(draw, name);
+    }
+
+    View.OnClickListener mango2list = new View.OnClickListener()
     {
         @Override
         public void onClick(View v)
         {
-         startMango1();
+            int draw = R.drawable.mango2;
+            String name = getResources().getString(R.string.mango2);
+            setActiveMango(draw, name);
         }
     };
-    public void startMango1()
-    {
-        String activeMango;
-        Intent openMango1 = new Intent(this , ImageDisplayActivity.class);
-        startActivity(openMango1);
-    }
 
+    View.OnClickListener mango3list = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            int draw = R.drawable.mango3;
+            String name = getResources().getString(R.string.mango3);
+            setActiveMango(draw, name);
+        }
+    };
+
+    View.OnClickListener mango4list = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            int draw = R.drawable.mango4;
+            String name = getResources().getString(R.string.mango4);
+            setActiveMango(draw, name);
+        }
+    };
+
+    //sets using data parsed in from the image clicks, sets the active image as the intent
+    //and starts the display activity
+    public void setActiveMango (int mangoInt , String mangoStr)
+    {
+        Intent openMango = new Intent(this , ImageDisplayActivity.class);
+        openMango.putExtra("drawMango", mangoInt);
+        openMango.putExtra("nameMango", mangoStr);
+        startActivity(openMango);
+    }
 
 }
