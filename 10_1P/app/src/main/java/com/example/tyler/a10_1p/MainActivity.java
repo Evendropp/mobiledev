1cdfce856d8629340aa6ad503196660f0da1f022
Even <tyler-smith@live.com.au>
2018-11-01 18:08:34 +1100

    Completed 10_1P

--- 10_1P/app/src/main/java/com/example/tyler/a10_1p/MainActivity.java
+++ 10_1P/app/src/main/java/com/example/tyler/a10_1p/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.tyler.a10_1p;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(buttlistener);
+    }
+
+    View.OnClickListener buttlistener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            new updateText().execute();
+        }
+    };
+
+    private class updateText extends AsyncTask<Void, Integer, Void>
+    {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                for (int i = 3; i >= 0; i--) {
+                    Thread.sleep(1000);
+                    publishProgress(i);
+                    Log.i("count",Integer.toString(i));
+                }
+            } catch (InterruptedException ie)
+
+            {
+                ie.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            TextView text = findViewById(R.id.textView);
+            text.setText(Integer.toString(values[0]));
+            super.onProgressUpdate(values);
+        }
     }
 }
